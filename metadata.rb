a30a9fdb91e74d5734e417e8494bd6029dd13f8c
Edmund Haselwanter <me@ehaselwanter.com>
2014-09-29 09:59:01 +0200

    initial apache hardening project files
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name             "apache-hardening"
+maintainer       "Edmund Haselwanter"
+maintainer_email "me@ehaselwanter.com"
+license          "Apache v2.0"
+description      "Installs and configures a secure apache server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends 'apache2', '~ 2.0.0'
+
+recipe 'apache-hardening::default', 'calls hardening recipe'
+recipe 'apache-hardening::hardening', 'add hardening configuration apache server'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
